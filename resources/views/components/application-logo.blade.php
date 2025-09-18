18df72d567c70e7b92c25bfe6ae94a08a130685d
payerrony <payerrony@gmail.com>
2025-09-18 13:32:43 +0600

    logo issue

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,2 +1,6 @@
 
-<img src="{{ setting('logo') }}" alt="{{ setting('site_name') }}" class="brand-image opacity-75 shadow" />
+<img 
+    src="{{ setting('logo') ?? 'https://placehold.co/32x32?text=Logo' }}" 
+    alt="{{ setting('site_name') ?? 'Company Logo' }}" 
+    class="{{ setting('logo') ? 'brand-image opacity-75 shadow' : 'w-20 h-20' }}"
+/>
